533ac74a0b16559a3196f8c6a0a90739bcc0eec3
daikato <dai666k@gmail.com>
2022-03-16 23:35:28 +0900

    再帰で二分探索

--- binarySearch.go
+++ binarySearch.go
@@ -4,19 +4,35 @@ import "fmt"
 
 
 func main() {
-  value := make([]int, 0, 60)
+  value := make([]int, 0, 101)
+  target := 0
   cap := cap(value)
   for len := len(value); len < cap; len++ {
-	  len+=2;
-	value = append(value, len)
+	  value = append(value, len)
+	  len++;
+	  len++;
   }
 
-  test := binarySearch(5,value)
-  fmt.Println(test)  
+  answerIndex := binarySearchByRecursion(value,target,60,0, len(value)/2)
+  fmt.Println(answerIndex)
+  if(answerIndex > 0){
+	  fmt.Println(value[answerIndex] == target)
+  }
 }
 
-//TODO: 再帰関数化&ロジックを修正
-func binarySearch(target int, valueSlice []int) int{
+func binarySearchByRecursion(valueSlice []int, target int, right int, left int, mid int) int{
+  if(right > left && mid != left && mid != right){
+	  if(valueSlice[mid] == target){
+		  return mid
+	  }
+	  if(valueSlice[mid] < target){
+		  return binarySearchByRecursion(valueSlice, target, right, mid,(right + mid)/2)
+		}
+		return binarySearchByRecursion(valueSlice, target, mid, left, (mid - left)/2)
+	}
+  return -1;
+}
+func binarySearchByRoop(target int, valueSlice []int) int{
   length := len(valueSlice)
   mid := length /2
   right := length
